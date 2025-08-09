a48be310b554141b35b43b5fd29bfcb758aac70b
TanveenAmbrose <racoctanveen15@gmail.com>
2025-08-09 08:36:05 +0600

    ok

--- practiceExpress/app.js
+++ practiceExpress/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const express = require('express');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 
 const homeRouter = require('./routes/homeRouter');
